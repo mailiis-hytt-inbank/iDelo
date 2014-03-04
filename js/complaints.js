0ad9e1c732c3e44f473ca7791e6ab6310d84cb35
Mai-iis <mailiish@gmail.com>
2014-03-04 11:21:04 +0200

    validation not ready

--- js/complaints.js
+++ js/complaints.js
@@ -1,4 +1,23 @@
 $(function(){
-  $(".table").tablesorter();
+  $('.table').tablesorter();
   $('.search-box').val('');
+  $('.show-by-type').validate({
+    rules: {
+      SelectName: { valueNotEquals: "default" }
+    },
+    messages: {
+      SelectName: { valueNotEquals: "Please select an item!" }
+    },
+    highlight: function(element, errorClass, validClass) {
+      $(element).parent().addClass('has-error').removeClass(validClass);
+    },
+    unhighlight: function(element, errorClass, validClass) {
+      $(element).parent().removeClass('has-error').addClass(validClass);  
+    },
+    success: function(label) {
+      label.parent().addClass('has-success');
+      label.parent().children('span').addClass('form-control-feedback');
+      label.addClass('hidden-label');
+    }
+  });
 });
