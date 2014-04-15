315ff69162f371e8cac37026f9b7feec9834b2db
Mai-Liis Hütt <mailiish@gmail.com>
2014-04-15 21:01:20 +0300

    add complaint form

--- js/add_complaint.js
+++ js/add_complaint.js
@@ -35,4 +35,30 @@ $(function(){
       label.addClass('hidden-label');
     }
   });
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost/cgi-bin/iDelo/get_types.py',
+    dataType: 'json',
+    success: function(data){
+      var rows = {rows: data};
+      var theTemplateScript = Templates.current_types;  
+       var theTemplate = Handlebars.compile(theTemplateScript);  
+      $('.current-types').append(theTemplate(rows));
+      $('.current-types-select').attr('name', "type")
+    }
+  });
+  $('.add-field').on('click', function(e){
+    e.preventDefault();
+    var field = "<div class='form-group'><label class='control-label col-sm-2 margin-right'></label><div class='col-sm-8'><input class='form-control field' name='picture' type='text' value='"+$('.new-field-data').val()+"'></div><span class='glyphicon glyphicon-remove remove-item'></span></div>";
+    $('.images').append(field);
+    $('.new-field-data').val("");
+  });
+  $('.new-complaint').on('click', '.remove-item', function(e){
+     e.preventDefault();
+    $(this).parent('.form-group').remove();
+  });
+  $('.registrate').on('click', function(e){
+    even.preventDefault();
+    
+  })
 });
--- js/map.js
+++ js/map.js
@@ -1,4 +1,31 @@
-$(function(){
-  
+$(function(){ 
+  var lat = 47.791766;
+  var log = 29.416494;
+  var map_canvas = document.getElementById('map_canvas');
+  var map_options = {
+    center: new google.maps.LatLng(lat, log),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(map_canvas, map_options);
+  var myLatlng = new google.maps.LatLng(lat, log);
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    draggable:true,
+    animation: google.maps.Animation.DROP
+  });
+    $('.latitude').val(lat);
+    $('.longtitude').val(log);
+
+
+  google.maps.event.addListener(marker, 'dragend', function(evt){
+    // TODO: on submit get coordinates
+    console.log(evt.latLng.lat());
+    $('.latitude').val(evt.latLng.lat());
+    console.log(evt.latLng.lng());
+    $('.longtitude').val(evt.latLng.lng());
+
+});
 });
 
